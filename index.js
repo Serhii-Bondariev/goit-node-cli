574d7ce843c32bd465cfbf8a4a93f13d2aaef863
Serhii <xx.code4u@gmail.com>
2024-02-22 22:24:37 +0200

    bbbbb

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
 import { program } from "commander";
+import * as contacts from "./contacts.js";
+
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option(
+    "-a, --action <string>",
+    "Дія, яку потрібно виконати (list, get, add, remove)"
+  )
+  .option("-i, --id <string>", "Ідентифікатор контакту")
+  .option("-n, --name <string>", "Ім'я контакту")
+  .option("-e, --email <string>", "Електронна пошта контакту")
+  .option("-p, --phone <string>", "Номер телефону контакту");
 
 program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
+  try {
+    if (!action) {
+      throw new Error("Не вказано дію!");
+    }
+    switch (action.toLowerCase()) {
+      case "list":
+        const contactsList = await contacts.listContacts();
+        console.table(contactsList);
+        break;
 
-    case "get":
-      // ... id
-      break;
+      case "get":
+        const contact = await contacts.getContactById(id);
+        if (contact) {
+          console.log(contact);
+        } else {
+          console.warn("Контакт не знайдений!");
+        }
+        break;
 
-    case "add":
-      // ... name email phone
-      break;
+      case "add":
+        const newContact = await contacts.addContact(name, email, phone);
+        console.log(newContact);
+        break;
 
-    case "remove":
-      // ... id
-      break;
+      case "remove":
+        const removedContact = await contacts.removeContact(id);
+        if (removedContact) {
+          console.log(removedContact);
+        } else {
+          console.warn("Контакт не знайдений!");
+        }
+        break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      default:
+        throw new Error("Невідомий тип дії!");
+    }
+  } catch (error) {
+    console.error("Помилка:", error.message);
   }
 }
 
